2ba6422762bd06f8c2fb06e775d0b5acebb1ff00
Timur <abubakirov@yandex.ru>
2020-12-22 15:54:55 +0300

    no color filter added

--- src/components/ProductFilter.vue
+++ src/components/ProductFilter.vue
@@ -45,6 +45,24 @@
       <fieldset class="form__block">
         <legend class="form__legend">Цвет</legend>
         <ul class="colors">
+          <li class="colors__item">
+            <label class="colors__label">
+              <input class="colors__radio sr-only" type="radio" name="color"
+                     value="None" @click="currentColor = 'None'">
+              <span class="colors__value" style="background-color: #9d9d9d">
+                <svg xmlns="http://www.w3.org/2000/svg"
+                     width="20"
+                     height="20"
+                     fill="currentColor"
+                     class="bi bi-x"
+                     viewBox="0 0 20 20">
+                  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1
+                        .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646
+                        2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                </svg>
+              </span>
+            </label>
+          </li>
           <li class="colors__item" v-for="color in colors" :key="color">
             <label class="colors__label">
               <input class="colors__radio sr-only" type="radio" name="color"
